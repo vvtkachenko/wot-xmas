65e6083f63f2aef22994ad4f786f06b3aed7b66a
Volodymyr Tkachenko <tkachenk.v@gmail.com>
2021-12-19 20:34:45 +0200

    fix build

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/randomizer'),
+      dir: require('path').join(__dirname, './coverage/wot-xmas'),
       subdir: '.',
       reporters: [
         { type: 'html' },
